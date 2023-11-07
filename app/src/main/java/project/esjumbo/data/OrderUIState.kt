f22cf86b9aeb1d060c097294d65d5a5aee3c631e
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-11-07 10:58:05 +0700

    Initial commit

---
+++ app/src/main/java/project/esjumbo/data/OrderUIState.kt
@@ -0,0 +1,7 @@
+package project.esjumbo.data
+
+data class OrderUIState(
+    val jumlah: Int = 0,
+    val rasa: String = "",
+    val harga: String = "",
+)
---
+++ app/src/main/java/project/esjumbo/data/SumberData.kt
@@ -0,0 +1,13 @@
+package project.esjumbo.data
+
+import project.esjumbo.R
+
+object SumberData {
+    val flavors = listOf(
+        R.string.natural,
+        R.string.vanilla,
+        R.string.lemon,
+        R.string.milk,
+        R.string.lyche
+    )
+}

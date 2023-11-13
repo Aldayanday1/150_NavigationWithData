6fd0060c930db85ef76e539d402a299b059f6c50
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-11-13 18:33:48 +0700

    Add var listData with mutableListOf

--- app/src/main/java/project/esjumbo/HalamanForm.kt
+++ app/src/main/java/project/esjumbo/HalamanForm.kt
@@ -2,6 +2,10 @@ package project.esjumbo
 
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -9,4 +13,15 @@ fun HalamanForm(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackButtonClicked: () -> Unit,
 ) {
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var tlpnTxt by remember {
+        mutableStateOf( "")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, tlpnTxt)
+
 }

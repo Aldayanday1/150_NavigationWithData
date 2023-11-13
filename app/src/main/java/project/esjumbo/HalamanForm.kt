4001229810ebfa7b6be79737a77e522b25117659
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-11-13 18:21:33 +0700

    Add File 'HalamanForm' with Composable & OptIn, and function in it

---
+++ app/src/main/java/project/esjumbo/HalamanForm.kt
@@ -0,0 +1,11 @@
+package project.esjumbo
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(
+
+) {
+}

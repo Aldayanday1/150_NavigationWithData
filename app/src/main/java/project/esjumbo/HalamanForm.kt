539778593496d55172efc99b51da27feaa3ff710
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-11-13 19:36:01 +0700

    Add OutlinedTextField with the values name 'namaTxt, tlpnTxt, alamatTxt' & Spacer in it

--- app/src/main/java/project/esjumbo/HalamanForm.kt
+++ app/src/main/java/project/esjumbo/HalamanForm.kt
@@ -1,11 +1,24 @@
 package project.esjumbo
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,4 +37,40 @@ fun HalamanForm(
     }
     var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, tlpnTxt)
 
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Text(text = "Data Pelanggan", modifier = Modifier.padding(bottom = 25.dp))
+
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {namaTxt = it},
+            label = { Text(text = stringResource(id = R.string.nama))},
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedTextField(
+            value = tlpnTxt,
+            onValueChange = {tlpnTxt = it},
+            label = { Text(text = stringResource(id = R.string.notlp))},
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {alamatTxt = it},
+            label = { Text(text = stringResource(id = R.string.alamat))},
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp)
+        )
+
+        Spacer(modifier = Modifier.height(35.dp))
+    }
 }

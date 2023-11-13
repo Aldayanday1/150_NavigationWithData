0d18dba8107bf36815acc8cf06aae8dbc05726f9
Mochamad Aldi Raihan Fachrizal <onlymarfa69@mail.umy.ac.id>
2023-11-13 18:26:49 +0700

    Add onSubmitButtonClicked with the MutableList 'String' & onBackButtonClicked to Unit

--- app/src/main/java/project/esjumbo/HalamanForm.kt
+++ app/src/main/java/project/esjumbo/HalamanForm.kt
@@ -6,6 +6,7 @@ import androidx.compose.runtime.Composable
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonClicked: () -> Unit,
 ) {
 }
